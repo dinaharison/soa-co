01e67488fc209010b77bbf727ee311ca3b8df484
dinaharison <dinaharison.jb@gmail.com>
2024-08-21 13:55:35 +0300

    navbar responsive collasped works - added ngbmodule

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,16 @@ import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavItemComponent } from '../nav-item/nav-item.component';
 import { CommonModule } from '@angular/common';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, NavItemComponent, CommonModule],
+  imports: [RouterModule, NavItemComponent, CommonModule, NgbCollapseModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   @Input() routes: { route: string; name: string }[] = [];
+  isMenuCollapsed = true;
 }
